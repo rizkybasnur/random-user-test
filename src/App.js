dafe27eef49e1da13d386db00a616a6edfcab2d1
Rizky Basnur <rizkyrahmansyahbasnur@gmail.com>
2021-08-26 12:54:34 +0700

    get api completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import "./App.scss";
 import { randomSrvc } from "./service/service";
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 import { randomSuccess } from "./redux/action/randomAct";
 import { useState } from "react";
 
 function App() {
-  const dispatch = useDispatch();
-  const { pict } = useSelector((state) => state.randomred);
+  const [pict, setpict] = useState("");
+  const [nama, setnama] = useState("");
+  const [email, setemail] = useState("");
+  const [alamat, setalamat] = useState("");
+  // const dispatch = useDispatch();
+  // const { pict } = useSelector((state) => state.randomred);
   const handleClick = () => {
     randomSrvc()
       .then((response) => {
         console.log(response);
-        const { pict } = response.data.data.results[0].picture.medium;
-        dispatch(randomSuccess(pict));
+        setpict(response.data.results[0].picture.medium);
+        setnama(response.data.results[0].name.first);
+        setemail(response.data.results[0].email);
+        setalamat(response.data.results[0].location.city);
       })
       .catch((error) => {
         console.log(error);
@@ -25,9 +31,9 @@ function App() {
           <img src={pict} alt=""></img>
         </div>
         <div className="data">
-          <h4 className="nama">nama</h4>
-          <h4 className="email">email</h4>
-          <h4 className="alamat">alamat</h4>
+          <h4 className="nama">{nama}</h4>
+          <h4 className="email">{email}</h4>
+          <h4 className="alamat">{alamat}</h4>
         </div>
         <button onClick={handleClick}>next</button>
       </div>
